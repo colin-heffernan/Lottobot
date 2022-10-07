1f5ab43f71d2b9320af0889fd718b5bf4c78618d
Colin Heffernan <colinpheffernan@gmail.com>
2022-10-07 15:36:42 -0400

    Begin to implement lottery system

--- src/classes/event.ts
+++ src/classes/event.ts
@@ -1,8 +1,10 @@
+import { ExtendedClient } from "./extendedClient";
+
 export class Event {
 	eventName: string;
 	once: boolean;
-	execute: (...args: any) => void;
-	constructor(eventName: string, execute: (...args: any) => void, once: boolean) {
+	execute: (client: ExtendedClient, ...args: any) => void;
+	constructor(eventName: string, execute: (client: ExtendedClient, ...args: any) => void, once: boolean) {
 		this.eventName = eventName;
 		this.execute = execute;
 		this.once = once;
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,21 @@
-import { Client } from "discord.js";
+import { pickWinner } from "../pickWinner";
 import { Event } from "../classes/event";
+import { ExtendedClient } from "../classes/extendedClient";
 
 export const event = new Event("ready",
-	function execute(client: Client) {
+	function execute(client: ExtendedClient) {
+		client.user?.setActivity("the numbers");
 		console.log(`Logged in as ${client.user?.tag}.`);
-		console.log(`Watching ${client.guilds.cache.size} servers.`);
+		console.log(`Monitoring ${client.commands.size} commands.`);
+		console.log(`Monitoring ${client.guilds.cache.size} servers.`);
 		console.log("Ready!");
+
+		setInterval(() => {
+			const date = new Date();
+			if (date.getHours() == 15 && date.getMinutes() == 32) {
+				pickWinner(client);
+			}
+		}, 60000);
 	},
 	true,
 );
