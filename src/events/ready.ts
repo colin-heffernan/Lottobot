15fca4c3edb6549f4af86a19380f92c4c8b4011b
Colin Heffernan <colinpheffernan@gmail.com>
2022-10-09 00:31:39 -0400

    Check if client is logged in as a user

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,8 +6,13 @@ import { ExtendedClient } from "../classes/extendedClient";
 export const event = new Event("ready",
 	// Define the execute function
 	(client: ExtendedClient) => {
-		client.user?.setActivity("the numbers");
-		console.log(`Logged in as ${client.user?.tag}.`);
+		if (client.user) {
+			client.user.setActivity("the numbers");
+			console.log(`Logged in as ${client.user.tag}.`);
+		}
+		else {
+			console.log("Not logged in as any user.");
+		}
 		console.log(`Monitoring ${client.getCommands().size} commands.`);
 		console.log(`Monitoring ${client.guilds.cache.size} servers.`);
 		console.log("Ready!");
