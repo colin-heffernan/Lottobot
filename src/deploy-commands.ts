2ebc095483fb0b896deb04b669afbea14693190f
Colin Heffernan <colinpheffernan@gmail.com>
2022-10-15 22:40:13 -0400

    Fix command deployer

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -6,24 +6,35 @@ import { REST } from "@discordjs/rest";
 import { clientId, guildId, token } from "./config.json";
 
 // Declare and initialize variables that will be used later
-// TODO: Stop using any[] for commands type
-const commands: any[] = [];
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".ts"));
+(async () => {
+	const commands: string[] = [];
+	const commandsPath = path.join(__dirname, "commands");
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".ts"));
 
-// Populate the commands array with commands from the ./commands/ directory
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	import(filePath).then(module => {
-		const command = module.command;
-		commands.push(command.getData().toJSON());
-	});
-}
+	// Populate the commands array with commands from the ./commands/ directory
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		await import(filePath).then(module => {
+			const command = module.command;
+			commands.push(command.getData().toJSON());
+		});
+	}
 
-// Honestly, I'm not entirely sure what happens after here
-// TODO: Stop using any for data type
-const rest = new REST({ version: "10" }).setToken(token);
+	// Honestly, I'm not entirely sure what happens after here
+	// TODO: Stop using any for data type
+	const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data: any) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+	await (async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application commands.`);
+			const data: any = await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: commands },
+			);
+			console.log(`Successfully registered ${data.length} application commands.`);
+		}
+		catch (error) {
+			console.error(error);
+		}
+	})();
+})();
